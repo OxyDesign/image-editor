208abe6d84d3c223812f8d09100d1bbb4fbd71fa
Nicolas Escoffier <nico.oxydesign@free.fr>
2015-07-30 23:40:52 +0100

    Settings, Tools & Structure

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+var gulp = require('gulp'),
+    rename = require('gulp-rename'),
+    coffee = require('gulp-coffee'),
+    uglify = require('gulp-uglify');
+
+// Scripts
+gulp.task('scripts', function() {
+  return gulp.src('src/*.coffee')
+    .pipe(coffee())
+    .pipe(uglify())
+    .pipe(rename({suffix: ".min"}))
+    .pipe(gulp.dest('dist/'));
+});
+
+// Default task
+gulp.task('default', function() {
+  gulp.watch('src/*.coffee', ['scripts']);
+});
+
+
